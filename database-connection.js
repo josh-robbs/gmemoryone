5bf4c5e54d7d59542c63a90814b47d490b69c827
Josh Robbs <joshrobbs@Zuul.local>
2018-07-30 13:27:26 -0600

    fixing seed file

--- database-connection.js
+++ database-connection.js
@@ -1,12 +1,12 @@
-// const knex = require('knex')
-// const environment = process.env.NODE_ENV || "development"
-// const config = require('./knexfile')
-// const database = require('knex')(config[environment])
+const knex = require('knex')
+const environment = process.env.NODE_ENV || "development"
+const config = require('./knexfile')
+const database = require('knex')(config[environment])
 
 
 
-// module.exports = database
+module.exports = database
 
 
-const CONFIG = require("./knexfile")[process.env.NODE_ENV || "development"];
-module.exports = require("knex")(CONFIG);
+// const CONFIG = require("./knexfile")[process.env.NODE_ENV || "development"];
+// module.exports = require("knex")(CONFIG);
