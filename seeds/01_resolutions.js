b7b69ff946a76add888a82a61032922fe1682faa
Josh Robbs <joshrobbs@Zuul.local>
2018-07-30 14:23:21 -0600

    fixing seeds

--- seeds/01_resolutions.js
+++ seeds/01_resolutions.js
@@ -1,8 +1,8 @@
 
 exports.seed = function(knex, Promise) {
-  return knex('student').del()
+  return knex('resolution').del()
     .then(() => {
-      return knex('student').insert([
+      return knex('resolution').insert([
         {dueDate:'1997-02-01', resolution:'Go skiing'},
         {dueDate:'1997-05-01', resolution:'Do stand-up'},
         {dueDate:'1997-09-01', resolution:'Start knitting'}
